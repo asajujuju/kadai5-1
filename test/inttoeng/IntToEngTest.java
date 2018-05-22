95484cc9ab43e28d270dc00c99f15338a6367d2c
asajujuju <g16955yf@gm.tsuda.ac.jp>
2018-05-22 14:19:25 +0900

    test

--- test/inttoeng/IntToEngTest.java
+++ test/inttoeng/IntToEngTest.java
@@ -1,36 +1,32 @@
 package inttoeng;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class IntToEngTest {
 
 	@Test
-	public void 二十以上の数字を英語に変換します() {
-		IntToEng ite = new IntToEng();
+	public void 二十以上数字を英語に変換します	(){
+		IntToEng ite  = new IntToEng();
 		String expected = "twenty";
 		String actual = ite.translateEng(20);
-		assertThat(actual,is(expected));
-		
+		assertThat(actual, is(expected));
 	}
 	@Test
-	public void 一桁を英語に変換します() {
-		IntToEng ite = new IntToEng();
-		String expected = "onehundredone";
-		String actual = ite.translateEng(101);
-		assertThat(actual,is(expected));
-		
+	public void 一桁を英語に変換します	(){
+		IntToEng ite  = new IntToEng();
+		String expected = "one";
+		String actual = ite.translateEng(1);
+		assertThat(actual, is(expected));
 	}
 	@Test
-	public void 十番台の数字を英語に変換します() {
-		IntToEng ite = new IntToEng();
+	public void 十番台数字を英語に変換します	(){
+		IntToEng ite  = new IntToEng();
 		String expected = "nineteen";
 		String actual = ite.translateEng(19);
-		assertThat(actual,is(expected));
-		
+		assertThat(actual, is(expected));
 	}
 
 }
